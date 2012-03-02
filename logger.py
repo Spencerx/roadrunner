7719a04d149c0de4ec3e4ac8d5102ed9e4573c05
Nikhil Singh <nix85@ymail.com>
2012-03-02 18:10:37 +0530

    Added License terms to each of the source code files

--- logger.py
+++ logger.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2012, Yahoo! Inc.  All rights reserved.
+# Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+#
+
 import logging
 
 #
